4021a535f9aff806ae7cde71889a0dd552da3a36
Armmanoj <132729991+Armmanoj@users.noreply.github.com>
2023-07-07 10:51:51 +0530

    Update stars_identifying.py

--- stars_identifying.py
+++ stars_identifying.py
@@ -5,14 +5,15 @@ import numpy as np
 
 # function to return centroid and FWHM from numpy array
 def star_centroids(data):
-    centroids=[[],[]]
-    FWHM=0
+    centroids=[[],[]] #x and y coordinates of the centroids of stars in the image
+    FWHM=0 # Full width Half maximum, the radius around centroid at which intensity roughly halves
+    # Insert code for finding centroid and FWHM
     return (centroids,FWHM)
 
 # displaying the location of stars
-filename="M-31Andromed220221022931.FITS"
-with fits.open(filename) as hdul:
-    data=hdul[0].data
-    centroids,FWHM=star_centroids(data)
-    plt.scatter(centroids[0],centroids[1],s=np.pi*FWHM*FWHM,c='tab:blue')
+filename="M-31Andromed220221022931.FITS" # this is an exaple file
+with fits.open(filename) as hdul: # returns a list of header-data-units 
+    data=hdul[0].datab# a header-data-unit object has a string(header) and a numpy array(image data)
+    centroids,FWHM=star_centroids(data) 
+    plt.scatter(centroids[0],centroids[1],s=np.pi*FWHM*FWHM,c='tab:blue') # scatter plot showing location of stars
     plt.show()
